667fa6b8f0d7819e478580080049fb9963b5056d
EvgenyGuguev <evgenygost@gmail.com>
2021-02-25 20:41:02 +0300

    DeleteEmployeeForCompany method

--- Common/Controllers/EmployeesController.cs
+++ Common/Controllers/EmployeesController.cs
@@ -91,5 +91,28 @@ namespace Common.Controllers
                 employeeToReturn
             );
         }
+        
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployeeForCompany(Guid companyId, Guid id)
+        {
+            var company = _repository.Company.GetCompany(companyId, false);
+            if (company == null)
+            {
+                _logger.LogInfo($"Company with id: {companyId} doesn't exist in the database.");
+                return NotFound();
+            }
+
+            var employeeForCompany = _repository.Employee.GetEmployee(companyId, id, false);
+            if (employeeForCompany == null)
+            {
+                _logger.LogInfo($"Employee with id: {id} doesn't exist in the database.");
+                return NotFound();
+            }
+            
+            _repository.Employee.DeleteEmployee(employeeForCompany);
+            _repository.Save();
+
+            return NoContent();
+        }
     }
 }
