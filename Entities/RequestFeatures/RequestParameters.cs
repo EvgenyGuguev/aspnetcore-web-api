4d30d59bbbe60f3a81751f109a15ef2d8c265952
EvgenyGuguev <evgenygost@gmail.com>
2021-03-02 21:36:21 +0300

    work with sorting

--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -12,5 +12,7 @@ namespace Entities.RequestFeatures
             get => _pageSize;
             set => _pageSize = value > MaxPageSize ? MaxPageSize : value;
         }
+        
+        public string OrderBy { get; set; }
     }
 }
--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,6 +1,10 @@
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 using Entities.Models;
+using System.Linq.Dynamic.Core;
+using Repository.Extensions.Utility;
 
 namespace Repository.Extensions
 {
@@ -16,6 +20,17 @@ namespace Repository.Extensions
             var lowerCaseTerm = searchTerm.Trim().ToLower();
 
             return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString)) return employees.OrderBy(e => e.Name);
+            
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+            
+            if (string.IsNullOrWhiteSpace(orderQuery)) return employees.OrderBy(e => e.Name);
+
+            return employees.OrderBy(orderQuery);
         }
     }
 }
